992919a0f2767532328ca42e8079e6edbbe3b289
root <amuMabirimise>
2023-10-12 06:53:52 -0700

    Base_model

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''
+This is a base model that defines all common attributes and
+methods of other classes
+'''
+from datetime import datetime
+#from models import storage
+import uuid
+
+
+class BaseModel:
+    '''
+    represents the base model class of AirBnB console
+    '''
+
+    def __init__(self, *args, **kwargs):
+        '''
+        constructor
+        '''
+        from models import storage
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            for i, j in kwargs.items():
+                if "__class__" not in i:
+                    setattr(self, i, j)
+
+    def __str__(self):
+        """String repr of the Base Model Class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """
+            Returns a dictionary containing all
+            keys/values of __dict__ of the instance
+        """
+        bModelDict = dict(self.__dict__)
+        bModelDict['__class__'] = type(self).__name__
+        bModelDict['created_at'] = self.created_at.isoformat()
+        bModelDict['updated_at'] = self.updated_at.isoformat()
+        return bModelDict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+'''
+serialized and deserialized instances to a json file
+'''
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    '''
+    fillestorage class representation
+    '''
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''
+        returns all dictionary
+        '''
+        return self.__objects
+        
+    def new(self, obj):
+        """sets in __objects the obj
+        with key <obj class name>.id"""
+        
+        #key = obj.to_dict()['__class__'] + "." + obj.id
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+    
+    def save(self):
+        '''
+        converting python data to JSON
+        data structure
+        '''
+
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding="utf-8") as write_file:
+            json.dump(my_dict, write_file)
+
+    def reload(self):
+        '''
+        Deserializes the JSON file to __objects (if the JSON file exists).
+        If the file doesn't exist or any other error occurs, do nothing.
+        '''
+        try:
+            with open(self.__file_path, "r") as read_file:
+                data = json.load(read_file)
+                for key, value in data.items():
+                    #if key not in self.__objects:
+                     #   if value['__class__'] == 'User':
+                    self.__objects[key] = BaseModel(**value)
+                    #self.new(eval(key.split(".")[0])(**value))  
+        except (FileNotFoundError, json.JSONDecodeError, TypeError):
+            # Handle file not found, JSON decoding error, or unexpected value types
+            pass
